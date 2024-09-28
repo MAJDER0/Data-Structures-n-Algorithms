9632cdbdc9b6a49decc838fee9297372d0a1522b
MAJDER0 <tomasz.majder89@gmail.com>
2024-09-28 13:22:04 +0200

    Bubble Sort

---
+++ Data-Structures-n-Algorithms/Program.cs
@@ -0,0 +1,37 @@
+﻿using Data_Structures_n_Algorithms.Sorting_Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_n_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* ---------- Bubble Sort ---------- */
+
+            //int[] listOfNumbers = new int[] {23,4,2,6,21,6,2,76,2,5467};
+            //string[] listOfFruits = { "banana", "apple", "pear", "orange" };
+
+            //Bubble_Sort.Bubble(listOfNumbers); //Number Sorting - Standard Implementation
+            //Bubble_Sort.Bubble(listOfFruits); //Generic Sorting
+
+            //foreach (int number in listOfNumbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //Console.WriteLine("\n");
+
+            //foreach (var fruit in listOfFruits)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+        }
+    }
+}
---
+++ Data-Structures-n-Algorithms/Sorting Algorithms/Bubble_Sort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_n_Algorithms.Sorting_Algorithms
+{
+    public class Bubble_Sort
+    {
+
+        // 5 6 1 3 --> 5 1 6 3 --> 5 1 3 6 --> 1 5 3 6 --> 1 3 5 6 
+
+        //Time Complexity: O(n^2) worst, O(n^2) average, O(n) best
+        //Space Complexity: O(1)
+
+        //Generic implementation for any type
+
+        public static T[] Bubble<T>(T[] listOfItems) where T: IComparable<T> {
+
+            bool swaped;
+
+            for (int i = 0; i < listOfItems.Length; i++)
+            {
+                swaped = false;
+
+                for (int j = 0;j < listOfItems.Length-1-i; j++)
+                {
+                    if (listOfItems[j].CompareTo(listOfItems[j+1])>0)
+                    {
+                        swaped = true;
+                        (listOfItems[j], listOfItems[j + 1]) = (listOfItems[j+1], listOfItems[j]); //swap elements
+                    }
+                }
+
+                if (!swaped) {
+                    break; //If no swaps occured, the list is already sorted
+                }
+
+            }
+
+            return listOfItems;
+        }
+
+        //Implementation for the list of ints
+
+        public static int[] Bubble(int[] listOfNumbers) {
+
+            bool swaped;
+
+            for (int i = 0; i < listOfNumbers.Length-1; i++)
+            {
+                swaped = false;
+
+                for (int j = 0; j < listOfNumbers.Length-1-i; j++)
+                {
+                    if (listOfNumbers[j] > listOfNumbers[j + 1]) 
+                    {
+                        swaped = true;
+                        (listOfNumbers[j],listOfNumbers[j + 1]) = (listOfNumbers[j+1],listOfNumbers[j]); //swap elements
+                    
+                    }
+                }
+
+                if (!swaped)
+                {
+                    break; //If no swaps occured, the list is already sorted
+                }
+            }
+
+            return listOfNumbers;
+        }
+    }
+}

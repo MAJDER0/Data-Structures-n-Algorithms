06c48e4eff7d54d42503b870a11f45f32d48788e
MAJDER0 <tomasz.majder89@gmail.com>
2024-10-27 23:05:19 +0100

    Shell Sort

--- Data-Structures-n-Algorithms/Algorithms/Sorting Algorithms/Shell Sort.cs
+++ Data-Structures-n-Algorithms/Algorithms/Sorting Algorithms/Shell Sort.cs
@@ -8,24 +8,54 @@ namespace Data_Structures_n_Algorithms.Algorithms.Sorting_Algorithms
 {
     public class Shell_Sort
     {
-        public static void Shell(float[] arrayOfNumbers) {
+        //Time Comlexity:
 
-            int n = arrayOfNumbers.Length;
+        //Worst Case: O(n^2)
+        //Average Case: O(n^2)
+        //Best Case: O(nlogn)
 
-            for (int gap = n / 2; gap > 0; gap /= 2)
-            {
-                for (int i = gap; i < n; i++)
-                {
-                    float temp = arrayOfNumbers[i];
-                    int j = i;
-                    while (j >= gap && arrayOfNumbers[j - gap] > temp)
-                    {
-                        arrayOfNumbers[j] = arrayOfNumbers[j - gap];
-                        j -= gap;
-                    }
+        // ** gap sequence: n/2 where n is the number of elements in the array
+
+        //Space Complexity: O(1)
+
+        //Implementation for an array of integers
+        static void Shell(int[] arrayOfNumbers)
+        {
+            int length = arrayOfNumbers.Length;
+
+            for (int gap = length / 2; gap > 0; gap /= 2) {
+
+                for (int i = gap; i < length; i++) {
+                    
+                    int temp = arrayOfNumbers[i];
+                    int j;
+
+                    for (j = i; j >= gap && arrayOfNumbers[j-gap] > temp ; j-=gap) 
+                        arrayOfNumbers[j] = arrayOfNumbers[j-gap];
+
                     arrayOfNumbers[j] = temp;
                 }
             }
+        }
+
+        //Generic Implementation for any Type
+        static void Shell<T>(T[] arrayOfItems) where T : IComparable<T> {
+            
+            int length = arrayOfItems.Length;
+
+            for (int gap = length / 2; gap > 0; gap /= 2) {
+
+                for (int i = gap; i < length; i++) {
+                    
+                    T temp = arrayOfItems[i];
+                    int j;
+
+                    for (j = i; j >= gap && arrayOfItems[j - gap].CompareTo(temp) > 0; j -= gap)
+                        arrayOfItems[j] = arrayOfItems[j-gap];
+
+                    arrayOfItems[j] = temp;
+                }
+            }    
         }
     }
 }
---
+++ Data-Structures-n-Algorithms/properties/AssemblyInfo.cs.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Data-Structures-n-Algorithms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Data-Structures-n-Algorithms")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components. If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5b5a0c9-1e92-49a5-95da-2eac523e5f73")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

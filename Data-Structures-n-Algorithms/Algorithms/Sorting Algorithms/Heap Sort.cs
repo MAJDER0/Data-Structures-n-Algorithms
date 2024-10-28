4a9e45cb55030b0bd03f4e9053cf14778eb20709
MAJDER <tomasz.majder89@gmail.com>
2024-10-28 19:49:38 +0100

    Heap Sort

---
+++ Data-Structures-n-Algorithms/Algorithms/Sorting Algorithms/Heap Sort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_n_Algorithms.Algorithms.Sorting_Algorithms
+{
+    public class Heap_Sort
+    {
+       public static void Heap(int[] arr)
+       {
+            int n = arr.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(arr, n, i);
+            }
+
+            for (int i = n - 1; i > 0; i--)
+            {
+
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Heapify(arr, i, 0);
+            }
+       }
+
+        private static void Heapify(int[] arr, int n, int i)
+        {
+
+            int largest = i;
+
+            int l = 2 * i + 1;
+
+            int r = 2 * i + 2;
+
+            if (l < n && arr[l] > arr[largest])
+            {
+                largest = l;
+            }
+
+            if (r < n && arr[r] > arr[largest])
+            {
+                largest = r;
+            }
+
+            if (largest != i)
+            {
+                int temp = arr[i]; 
+                arr[i] = arr[largest];
+                arr[largest] = temp;
+
+                Heapify(arr, n, largest);
+            }
+        }
+    }
+}

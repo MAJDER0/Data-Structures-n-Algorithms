8c3eb8c7b86b39fa380b066228d222d6798ecc5b
MAJDER0 <tomasz.majder89@gmail.com>
2024-11-09 23:13:58 +0100

    Generic Implementation for Heap Sort

--- Data-Structures-n-Algorithms/Algorithms/Sorting Algorithms/Heap Sort.cs
+++ Data-Structures-n-Algorithms/Algorithms/Sorting Algorithms/Heap Sort.cs
@@ -21,6 +21,7 @@ namespace Data_Structures_n_Algorithms.Algorithms.Sorting_Algorithms
        {
             int length = arrayOfNumbers.Length;
 
+            //Build max heap
             for (int i = length / 2 - 1; i >=0; i--) {
 
                 Heapify(arrayOfNumbers,length,i);
@@ -53,6 +54,48 @@ namespace Data_Structures_n_Algorithms.Algorithms.Sorting_Algorithms
 
                 (arrayOfnumbers[currentNodeIndex], arrayOfnumbers[largest]) = (arrayOfnumbers[largest], arrayOfnumbers[currentNodeIndex]);
                 Heapify(arrayOfnumbers,length,largest);
+            }
+        }
+
+        //Generic Implementation for any Type
+
+        public static void Heap<T>(T[] arrayOfItems) where T : IComparable<T> {
+
+            int length = arrayOfItems.Count();
+
+            //Build max heap
+            for (int i = length / 2 - 1; i >= 0 ; i--) {
+
+                Heapify(arrayOfItems,length,i);
+
+            }
+
+            for (int i = length - 1; i > 0; i--) {
+
+                (arrayOfItems[i], arrayOfItems[0]) = (arrayOfItems[0], arrayOfItems[i]);
+                Heapify(arrayOfItems, i, 0);
+
+            }
+        }
+
+        private static void Heapify<T>(T[] arrayOfItems, int length, int currentNodeIndex) where T : IComparable<T> {
+
+            int largest = currentNodeIndex;
+
+            int left = 2 * currentNodeIndex + 1;
+            int right = 2 * currentNodeIndex + 2;
+
+            if (left < length && arrayOfItems[left].CompareTo(arrayOfItems[largest]) > 0)
+                largest = left;
+
+            if(right < length && arrayOfItems[right].CompareTo(arrayOfItems[largest]) > 0)
+                largest = right;
+
+            if (largest != currentNodeIndex) {
+
+                (arrayOfItems[largest], arrayOfItems[currentNodeIndex]) = (arrayOfItems[currentNodeIndex], arrayOfItems[largest]);
+                Heapify(arrayOfItems,length,largest);
+
             }
         }
     }

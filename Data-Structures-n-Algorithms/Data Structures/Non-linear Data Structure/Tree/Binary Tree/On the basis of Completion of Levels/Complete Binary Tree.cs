b7e54fb4ccdc03990f327650af407f094e4315c4
MAJDER0 <tomasz.majder89@gmail.com>
2024-11-11 21:22:26 +0100

    Complete Binary Tree

---
+++ Data-Structures-n-Algorithms/Data Structures/Non-linear Data Structure/Tree/Binary Tree/On the basis of Completion of Levels/Complete Binary Tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_n_Algorithms.Data_Structures.Non_linear_Data_Structure.Tree.Binary_Tree.On_the_basis_of_Completion_of_Levels
+{
+    public class Complete_Binary_Tree
+    {
+        //A complete binary tree is a special type of binary tree where all the levels of the tree are
+        //filled completely except the lowest level nodes which are filled from as left as possible
+
+        /* 
+          
+        Terminology of Complete Binary Tree:
+        
+
+        Root - Node in which no edge is coming from the parent
+
+        Child - Node having some incoming edge is called child
+
+        Sibling - Nodes having the same parent are sibling
+
+        Degree of a node - Number of children of a particular parent
+
+        Internal/External nodes - Leaf nodes are external nodes and non leaf nodes are internal nodes
+
+        Level - Count nodes in a path to reach a destination node.
+
+        Height - Number of edges to reach the destination node, Root is at height 0
+
+        */
+    }
+}

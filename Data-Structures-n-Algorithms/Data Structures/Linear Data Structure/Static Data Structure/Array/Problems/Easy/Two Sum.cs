14921c3710aaaea6f9530971049d4e8acdae8b4f
MAJDER <tomasz.majder89@gmail.com>
2024-12-07 23:42:29 +0100

    Two Sum Problem

---
+++ Data-Structures-n-Algorithms/Data Structures/Linear Data Structure/Static Data Structure/Array/Problems/Easy/Two Sum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_n_Algorithms.Data_Structures.Linear_Data_Structure.Static_Data_Structure.Array.Problems.Easy
+{
+    public class Two_Sum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                map[nums[i]] = i;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (map.ContainsKey(complement) && map[complement] != i)
+                {
+                    return new int[] { i, map[complement] };
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}

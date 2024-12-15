76472c479a16988163701eb0b88642bb0939ca06
MAJDER0 <tomasz.majder89@gmail.com>
2024-12-15 23:00:42 +0100

    Search Insert Position

---
+++ Data-Structures-n-Algorithms/Data Structures/Linear Data Structure/Static Data Structure/Array/Problems/Easy/Search Insert Position.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_n_Algorithms.Data_Structures.Linear_Data_Structure.Static_Data_Structure.Array.Problems.Easy
+{
+    public class Search_Insert_Position
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int pivot, left = 0, right = nums.Length - 1;
+            while (left <= right)
+            {
+                pivot = left + (right - left) / 2;
+                if (nums[pivot] == target)
+                    return pivot;
+                if (target < nums[pivot])
+                    right = pivot - 1;
+                else
+                    left = pivot + 1;
+            }
+
+            return left;
+        }
+    }
+}

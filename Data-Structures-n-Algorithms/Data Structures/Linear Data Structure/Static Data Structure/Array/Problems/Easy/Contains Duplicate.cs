fbc661483cdca65edf96a03b5879e71f5dbf3fbf
MAJDER0 <tomasz.majder89@gmail.com>
2024-12-16 21:45:10 +0100

    Contains Duplicate Problem

---
+++ Data-Structures-n-Algorithms/Data Structures/Linear Data Structure/Static Data Structure/Array/Problems/Easy/Contains Duplicate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_n_Algorithms.Data_Structures.Linear_Data_Structure.Static_Data_Structure.Array.Problems.Easy
+{
+    public class Contains_Duplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+
+            System.Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1]) return true;
+            }
+            return false;
+        }
+    }
+}

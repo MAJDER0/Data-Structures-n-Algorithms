6973b3471a17dd994b1b8c909a6aedc908be0ecd
MAJDER0 <tomasz.majder89@gmail.com>
2024-12-20 23:13:38 +0100

    Majority Element Problem

---
+++ Data-Structures-n-Algorithms/Data Structures/Linear Data Structure/Static Data Structure/Array/Problems/Easy/Majority Element.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_n_Algorithms.Data_Structures.Linear_Data_Structure.Static_Data_Structure.Array.Problems.Easy
+{
+    public class Majority_Element
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> Occurencies = new Dictionary<int, int>();
+
+            foreach (var value in nums)
+            {
+
+                if (Occurencies.ContainsKey(value))
+                {
+                    Occurencies[value]++;
+                }
+                else
+                {
+                    Occurencies.Add(value, 1);
+                }
+            }
+
+            int maxValue = 0;
+            int result = 0;
+
+            foreach (var znak in Occurencies)
+            {
+                if (maxValue < znak.Value)
+                {
+                    maxValue = znak.Value;
+                    result = znak.Key;
+                }
+            }
+            return result;
+        }
+    }
+}

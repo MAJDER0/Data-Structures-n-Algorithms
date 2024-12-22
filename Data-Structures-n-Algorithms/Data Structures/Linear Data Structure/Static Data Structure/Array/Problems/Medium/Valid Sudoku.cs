283460322e5b29b36e2079f92f779041b329c270
MAJDER <tomasz.majder89@gmail.com>
2024-12-22 12:47:00 -0700

    Valid Sudoku Problem

--- Data-Structures-n-Algorithms/Data Structures/Linear Data Structure/Static Data Structure/Array/Problems/Medium/Valid Sudoku.cs
+++ Data-Structures-n-Algorithms/Data Structures/Linear Data Structure/Static Data Structure/Array/Problems/Medium/Valid Sudoku.cs
@@ -10,22 +10,22 @@ namespace Data_Structures_n_Algorithms.Data_Structures.Linear_Data_Structure.Sta
     {
         public bool IsValidSudoku(char[][] board)
         {
-            int N = 9;
+            int size = 9;
 
-            HashSet<char>[] rows = new HashSet<char>[N];
-            HashSet<char>[] cols = new HashSet<char>[N];
-            HashSet<char>[] boxes = new HashSet<char>[N];
+            HashSet<char>[] rows = new HashSet<char>[size];
+            HashSet<char>[] cols = new HashSet<char>[size];
+            HashSet<char>[] boxes = new HashSet<char>[size];
 
-            for (int r = 0; r < N; r++)
+            for (int r = 0; r < size; r++)
             {
                 rows[r] = new HashSet<char>();
                 cols[r] = new HashSet<char>();
                 boxes[r] = new HashSet<char>();
             }
 
-            for (int r = 0; r < N; r++)
+            for (int r = 0; r < size; r++)
             {
-                for (int c = 0; c < N; c++)
+                for (int c = 0; c < size; c++)
                 {
                     char val = board[r][c];
 
